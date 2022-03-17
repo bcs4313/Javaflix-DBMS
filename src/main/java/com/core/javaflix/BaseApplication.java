160c990cabdd528d6052da2b2db31008b92ae375
bcs4313 <bcs4313@g.rit.edu>
2022-03-17 14:37:04 -0400

    Linked button events and textfields from the login fxml to BaseController.

--- src/main/java/com/core/javaflix/BaseApplication.java
+++ src/main/java/com/core/javaflix/BaseApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class BaseApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BaseApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(BaseApplication.class.getResource("login-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 852, 480); // standard 480p window size
+        stage.setTitle("JavaFlix");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/core/javaflix/BaseController.java
+++ src/main/java/com/core/javaflix/BaseController.java
@@ -1,14 +1,47 @@
 package com.core.javaflix;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class BaseController {
     @FXML
-    private Label welcomeText;
+    private Label emailAddressLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label passwordLabel;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML // contains email address info given by the user
+    private TextField emailAddressInput;
+
+    @FXML // contains email address info given by the user
+    private TextField passwordInput;
+
+    /**
+     * Listener for when the user clicks on the "Login" button
+     * Validate login information and load new page if valid, error message otherwise.
+     * @param actionEvent contains data regarding the nature of the interaction
+     */
+    @FXML
+    public void loginUser(ActionEvent actionEvent) {
+        System.out.println("Login Attempted");
+    }
+
+    /**
+     * Listener for when the user clicks on the "Create Account" button
+     * a new page is expected to load to enter info for the new account.
+     * @param actionEvent contains data regarding the nature of the interaction
+     */
+    @FXML
+    public void sendToAccountCreation(ActionEvent actionEvent) {
+        System.out.println("Create Account Button Clicked");
     }
 }
