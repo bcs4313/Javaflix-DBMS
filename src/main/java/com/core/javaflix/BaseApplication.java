66ecdc0186e2bc00edd128e7b03bc5146d8bb3d0
bcs4313 <bcs4313@g.rit.edu>
2022-03-17 15:35:14 -0400

    - Installed jcraft and sql dependencies - Created a DataStreamManager object that establishes a connection to the database with establishConnection(). Also statically stores the current session and connection made. - Updated BaseApplication to only load if a connection was successfully made. Otherwise an error message will print instead.

--- src/main/java/com/core/javaflix/BaseApplication.java
+++ src/main/java/com/core/javaflix/BaseApplication.java
@@ -6,15 +6,28 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class BaseApplication extends Application {
+    public static DataStreamManager dm;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(BaseApplication.class.getResource("login-page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 852, 480); // standard 480p window size
-        stage.setTitle("JavaFlix");
-        stage.setScene(scene);
-        stage.show();
+        // initialize a data stream to the JavaFlix database
+        DataStreamManager streamManager = new DataStreamManager();
+        if(streamManager.establishConnection()) // if the streamManager successfully connects
+        {
+            // load application page
+            FXMLLoader fxmlLoader = new FXMLLoader(BaseApplication.class.getResource("login-page.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 852, 480); // standard 480p window size
+            stage.setTitle("JavaFlix");
+            stage.setScene(scene);
+            stage.show();
+        }
+        else
+        {
+            System.err.println("Stopped Application Loading Process (connection failure)");
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module com.core.javaflix {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
+    requires java.sql;
+    requires jsch;
 
     opens com.core.javaflix to javafx.fxml;
     exports com.core.javaflix;
