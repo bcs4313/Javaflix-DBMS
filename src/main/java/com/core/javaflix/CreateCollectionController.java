197e0c3a51ae534a1691b55a82b0818897e391ca
Dylan <drs7727@g.rit.edu>
2022-03-19 13:36:04 -0400

    wrote insert collection sql statment

--- src/main/java/com/core/javaflix/CreateCollectionController.java
+++ src/main/java/com/core/javaflix/CreateCollectionController.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class CreateCollectionController {
 
@@ -11,14 +14,24 @@ public class CreateCollectionController {
     private Button cancelButton;
 
     @FXML
+    private TextField collectionNameInput;
+
+    @FXML
     private void sendToCollections() throws IOException {
         new CollectionWindow().load();
     }
 
     @FXML
-    private void createCollection()
-    {
-        System.out.println("Ligma");
+    private void createCollection() throws IOException, SQLException {
+        int id = AppStorage.userID;
+        String inputText = collectionNameInput.getText();
+        var c = DataStreamManager.conn;
+        System.out.println(c.getCatalog());
+        Statement statement = c.createStatement();
+        ResultSet rs = statement.executeQuery("INSERT INTO p320_05.\"Collection\" (\"CollectionID\", \"UserID\", \"CollectionName\") VALUES (2, " + id + ", \'" + inputText + "\')");
+        rs.next();
+        System.out.println(collectionNameInput.getText());
+        new CollectionWindow().load();
     }
 
 }
