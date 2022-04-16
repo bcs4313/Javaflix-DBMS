c76fbd309101e9e011e17ea77ad915a1fb1f10c2
bcs4313 <bcs4313@g.rit.edu>
2022-04-16 16:57:48 -0400

    Added extra information to display on the collections dashboard.

--- src/main/java/com/core/javaflix/dashboard/collections/CollectionController.java
+++ src/main/java/com/core/javaflix/dashboard/collections/CollectionController.java
@@ -9,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -20,7 +21,16 @@ import java.util.ArrayList;
 public class CollectionController {
 
     @FXML
-    private VBox collectionList;
+    private HBox CHBox;
+
+    @FXML
+    private VBox CollectionVBox;
+
+    @FXML
+    private VBox MCVBox;
+
+    @FXML
+    private VBox PlayLengthVBox;
 
     @FXML
     private Button createCollection;
@@ -78,16 +88,47 @@ public class CollectionController {
         var c = DataStreamManager.conn;
         System.out.println(c.getCatalog());
         Statement statement = c.createStatement();
-        ResultSet rs = statement.executeQuery("SELECT p320_05.\"Collection\".\"CollectionName\", p320_05.\"Collection\".\"CollectionID\" FROM p320_05.\"Collection\" WHERE \"UserID\" = \'" + userid + "\' ORDER BY p320_05.\"Collection\".\"CollectionName\" ASC");
+        ResultSet rs = statement.executeQuery("SELECT C.\"CollectionName\", C.\"CollectionID\", COUNT(CM.\"CollectionID\")\n" +
+                "FROM p320_05.\"CollectionMovie\" CM FULL OUTER JOIN\n" +
+                " p320_05.\"Collection\" C ON C.\"CollectionID\" = CM.\"CollectionID\"\n" +
+                "WHERE \"UserID\" = 9999\n" +
+                "group by C.\"CollectionName\", C.\"CollectionID\"\n" +
+                "ORDER BY C.\"CollectionName\" ASC");
         while(rs.next())
         {
             Button button = new Button(rs.getString("CollectionName"));
+            button.setPrefHeight(40);
             collectIDList.add(Integer.valueOf(rs.getString("CollectionID")));
             collectNameList.add(rs.getString("CollectionName"));
             button.setOnAction(buttonHandler);
-            collectionList.getChildren().add(button);
+            CollectionVBox.getChildren().add(button);
+            Label L = new Label(rs.getString("count"));
+            L.setPrefHeight(40);
+            MCVBox.getChildren().add(L);
         }
 
+        // looping through collection names locally and querying each time
+        for(int i = 1; i < CollectionVBox.getChildren().size(); i++) {
+            Button b = (Button)CollectionVBox.getChildren().get(i);
+            String collectionName = b.getText();
+            rs = statement.executeQuery("SELECT M.\"Duration\"\n" +
+                    "FROM p320_05.\"Collection\" C, p320_05.\"CollectionMovie\" CM, p320_05.\"Movie\" M\n" +
+                    "WHERE C.\"CollectionName\" = '" + collectionName + "'\n" +
+                    "AND CM.\"CollectionID\" = C.\"CollectionID\" AND\n" +
+                    "M.\"MovieID\" = CM.\"MovieID\"");
+            int totalHour = 0;
+            int totalMinute = 0;
+            while (rs.next()) {
+                String duration = rs.getString(1);
+                totalHour = totalHour + Integer.valueOf(duration.split("h")[0]);
+                totalMinute = totalMinute + Integer.valueOf(duration.split(" ")[1].split("m")[0]);
+            }
+            Label LL = new Label("Total Time: " + totalHour + "h " + totalMinute + "m");
+            LL.setPrefHeight(40);
+            PlayLengthVBox.getChildren().add(i, LL);
+        }
+
+        // total collection counting
         rs = statement.executeQuery("select COUNT(C.\"CollectionName\")\n" +
                 "from p320_05.\"Collection\" C\n" +
                 "where C.\"UserID\" = " + AppStorage.userID);
